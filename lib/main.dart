75cae66e30ec025ccba1b4d2fa65fa6f238fcb2c
brockeh8 <bfreiberger1@student.gsu.edu>
2025-09-15 18:17:27 -0400

    added tab code

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,240 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';   // standard lib // app bar, text, etc
 
-void main() {
-  runApp(const MyApp());
+
+
+void main() {   // main entry in flutter app 
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+
+class MyApp extends StatelessWidget {   
+  // main intro for objects to be used ie: a blueprint for whats to be built
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  Widget build(BuildContext context) {   // first widget 
+    return MaterialApp(     
+      // grabs all the common features to how we are theme, nav, and title
+      home: DefaultTabController(    
+        // default screen
+        length: 4,    // changed from 3 to 4
+        child: _TabsNonScrollableDemo(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class _TabsNonScrollableDemo extends StatefulWidget {   // extends widget class
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  __TabsNonScrollableDemoState createState() => __TabsNonScrollableDemoState();  
+  // assigns state to widget
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+
+class __TabsNonScrollableDemoState extends State<_TabsNonScrollableDemo>
+    with SingleTickerProviderStateMixin, RestorationMixin {
+  
+  late TabController _tabController;     // going to keep track of all tabs and there history
+  final RestorableInt tabIndex = RestorableInt(0);
+
+
+
+  @override
+  String get restorationId => 'tab_non_scrollable_demo';
+
+
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(tabIndex, 'tab_index');
+    _tabController.index = tabIndex.value;
+  }
+
+
+
+  @override
+  void initState() {
+    super.initState();   // called everytime something is created 
+    
+    _tabController = TabController(
+      initialIndex: 0,   // initialized 
+      length: 4,         
+      vsync: this,       // sync'd and pushes project to run smooth
+    );
+
+    _tabController.addListener(() {
+      setState(() {
+        tabIndex.value = _tabController.index;
+      });
     });
   }
 
+
+
+  // junk collector
+  @override
+  void dispose() {   // watches for leaks
+    _tabController.dispose();
+    tabIndex.dispose();
+    super.dispose();
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+    // For the To do task hint: consider defining the widget and name of the tabs here
+    final tabs = ['Tab 1', 'Tab 2', 'Tab 3', 'Tab 4']; 
+    
+
+    // app bar and body (main part of app)
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+        automaticallyImplyLeading: false,
+        title: Text('Tabs'),
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: false,
+          tabs: [
+            for (final tab in tabs) Tab(text: tab),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+
+          // TAB 1 
+          Container(
+            color: Colors.lightBlue.shade50,
+            padding: const EdgeInsets.all(16),
+
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                const Text(
+                  'Welcome',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+
+                const SizedBox(height: 12),
+
+                Card(
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text('Press for alert'),
+                  ),
+                ),
+
+                const SizedBox(height: 12),
+
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog<void>(
+                      context: context,
+                      builder: (ctx) => AlertDialog(
+                        title: const Text('Hello!'),
+                        content: const Text('AHHHHHHHHHH'),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(ctx),
+                            child: const Text('CLOSE'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  child: const Text('ALERT'),
+                ),
+              ],
+            ),
+          ),
+
+
+
+          // TAB 2
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'wsp',
+                  ),
+                ),
+                Image.network(
+                  'https://tse4.mm.bing.net/th/id/OIP.V2cHrtVIvnST5MKxARm2OwHaEK?cb=ucfimgc2&rs=1&pid=ImgDetMain&o=7&rm=3',
+                  width: 200,
+                  height: 200,
+                ),
+              ],
+            ),
+          ),
+
+
+          // TAB 3
+          Container(
+            color: Colors.orange.shade50,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(16),
+
+            child: ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('BUTTON YA!'),
+                  ),
+                );
+              },
+              child: Text('Click me plz'),
+            ),
+          ),
+
+
+
+          // TAB 4
+          Center( // ListView widget
+            child: ListView(
+              children: [
+                Card(
+                  child: ListTile(
+                    title: Text('1'),
+                    subtitle: Text('stuff'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Text('2'),
+                    subtitle: Text('more stuff'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Text('3'),
+                    subtitle: Text('dont look'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Text('4'),
+                    subtitle: Text('ENOUGH'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
