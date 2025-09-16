b4a220d6a7c995656996033f75c7910ee4f11efc
brockeh8 <bfreiberger1@student.gsu.edu>
2025-09-15 20:15:44 -0400

    Made mass commit for the weather class with the random number generator -- commands -- tab adds -- setting state -- string states

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
+import 'dart:math';
 import 'package:flutter/material.dart';   // standard lib // app bar, text, etc
-
-
 
 void main() {   // main entry in flutter app 
   runApp(MyApp());
 }
-
-
 
 class MyApp extends StatelessWidget {   
   // main intro for objects to be used ie: a blueprint for whats to be built
@@ -17,14 +14,12 @@ class MyApp extends StatelessWidget {
       // grabs all the common features to how we are theme, nav, and title
       home: DefaultTabController(    
         // default screen
-        length: 4,    // changed from 3 to 4
+        length: 7,    
         child: _TabsNonScrollableDemo(),
       ),
     );
   }
 }
-
-
 
 class _TabsNonScrollableDemo extends StatefulWidget {   // extends widget class
   @override
@@ -32,20 +27,44 @@ class _TabsNonScrollableDemo extends StatefulWidget {   // extends widget class
   // assigns state to widget
 }
 
-
-
 class __TabsNonScrollableDemoState extends State<_TabsNonScrollableDemo>
     with SingleTickerProviderStateMixin, RestorationMixin {
   
-  late TabController _tabController;     // going to keep track of all tabs and there history
+  late TabController _tabController;     
   final RestorableInt tabIndex = RestorableInt(0);
 
+  //weather start
+  final TextEditingController _cityController = TextEditingController();
+  String _city = '';
+  String _temp = '';
+  String _condition = '';
 
+  void _fetchWeather() {
+    final name = _cityController.text.trim();
+    if (name.isEmpty) {
+      setState(() {
+        _city = 'Please enter a city';
+        _temp = '';
+        _condition = '';
+      });
+      return;
+    }
+    //random num
+    final r = Random();
+    final t = 15 + r.nextInt(16);
+    const options = ['Sunny', 'Cloudy', 'Rainy'];
+    final cond = options[r.nextInt(options.length)];
+
+    setState(() {
+      _city = name;
+      _temp = '$t°C';
+      _condition = cond;
+    });
+  }
+  
 
   @override
   String get restorationId => 'tab_non_scrollable_demo';
-
-
 
   @override
   void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
@@ -53,16 +72,13 @@ class __TabsNonScrollableDemoState extends State<_TabsNonScrollableDemo>
     _tabController.index = tabIndex.value;
   }
 
-
-
   @override
   void initState() {
-    super.initState();   // called everytime something is created 
-    
+    super.initState();   
     _tabController = TabController(
-      initialIndex: 0,   // initialized 
-      length: 4,         
-      vsync: this,       // sync'd and pushes project to run smooth
+      initialIndex: 0,   
+      length: 7,         
+      vsync: this,       
     );
 
     _tabController.addListener(() {
@@ -72,30 +88,22 @@ class __TabsNonScrollableDemoState extends State<_TabsNonScrollableDemo>
     });
   }
 
-
-
-  // junk collector
   @override
-  void dispose() {   // watches for leaks
+  void dispose() {   
     _tabController.dispose();
     tabIndex.dispose();
+    _cityController.dispose();
     super.dispose();
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-
-    // For the To do task hint: consider defining the widget and name of the tabs here
-    final tabs = ['Tab 1', 'Tab 2', 'Tab 3', 'Tab 4']; 
+    final tabs = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']; 
     
-
-    // app bar and body (main part of app)
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: Text('Tabs'),
+        title: const Text('Tabs'),
         bottom: TabBar(
           controller: _tabController,
           isScrollable: false,
@@ -108,137 +116,11 @@ class __TabsNonScrollableDemoState extends State<_TabsNonScrollableDemo>
         controller: _tabController,
         children: [
 
-          // TAB 1 
+          //tab starts
           Container(
-            color: Colors.lightBlue.shade50,
-            padding: const EdgeInsets.all(16),
-
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-
-                const Text(
-                  'Welcome',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-
-                const SizedBox(height: 12),
-
-                Card(
-                  elevation: 3,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text('Press for alert'),
-                  ),
-                ),
-
-                const SizedBox(height: 12),
-
-                ElevatedButton(
-                  onPressed: () {
-                    showDialog<void>(
-                      context: context,
-                      builder: (ctx) => AlertDialog(
-                        title: const Text('Hello!'),
-                        content: const Text('AHHHHHHHHHH'),
-                        actions: [
-                          TextButton(
-                            onPressed: () => Navigator.pop(ctx),
-                            child: const Text('CLOSE'),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                  child: const Text('ALERT'),
-                ),
-              ],
-            ),
-          ),
-
-
-
-          // TAB 2
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'wsp',
-                  ),
-                ),
-                Image.network(
-                  'https://tse4.mm.bing.net/th/id/OIP.V2cHrtVIvnST5MKxARm2OwHaEK?cb=ucfimgc2&rs=1&pid=ImgDetMain&o=7&rm=3',
-                  width: 200,
-                  height: 200,
-                ),
-              ],
-            ),
-          ),
-
-
-          // TAB 3
-          Container(
-            color: Colors.orange.shade50,
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(16),
-
-            child: ElevatedButton(
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text('BUTTON YA!'),
-                  ),
-                );
-              },
-              child: Text('Click me plz'),
-            ),
-          ),
-
-
-
-          // TAB 4
-          Center( // ListView widget
-            child: ListView(
-              children: [
-                Card(
-                  child: ListTile(
-                    title: Text('1'),
-                    subtitle: Text('stuff'),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('2'),
-                    subtitle: Text('more stuff'),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('3'),
-                    subtitle: Text('dont look'),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('4'),
-                    subtitle: Text('ENOUGH'),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          )
         ],
       ),
     );
   }
 }
-
-
-
-// To do: Add a fourth tab with a ListView widget with at least 3 ListTile widgets in it.
